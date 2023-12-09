e2f7193e94bb49d7f25d17b98a8151e1d1607894
amaanlari <amaanlari@outlook.comm>
2023-12-09 16:34:09 +0530

    Debugged setup of view binding and connected bottom navigation with nav controller

--- app/src/main/java/com/lari/aml_news/ui/NewsActivity.kt
+++ app/src/main/java/com/lari/aml_news/ui/NewsActivity.kt
@@ -2,11 +2,26 @@ package com.lari.aml_news.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.lari.aml_news.R
+import com.lari.aml_news.databinding.ActivityNewsBinding
 
 class NewsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNewsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/lari/aml_news/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/lari/aml_news/ui/fragments/BreakingNewsFragment.kt
@@ -6,8 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.lari.aml_news.R
+import com.lari.aml_news.databinding.FragmentBreakingNewsBinding
 
 
 class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
 
+    private var _binding : FragmentBreakingNewsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentBreakingNewsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
