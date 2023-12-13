06a4691d2dc986e84189904a2babac10eda03f4d
amaanlari <amaanlari@outlook.comm>
2023-12-14 00:53:51 +0530

    Setup RecyclerView for the news articles.

---
+++ app/src/main/java/com/lari/aml_news/adapters/NewsAdapter.kt
@@ -0,0 +1,62 @@
+package com.lari.aml_news.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.lari.aml_news.databinding.ItemArticlePreviewBinding
+import com.lari.aml_news.models.Article
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+
+    inner class ArticleViewHolder(val binding: ItemArticlePreviewBinding): RecyclerView.ViewHolder(binding.root)
+
+    private val differCallback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        return ArticleViewHolder(
+            ItemArticlePreviewBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this).load(article.urlToImage).into(holder.binding.ivArticleImage)
+            holder.binding.tvSource.text = article.source.name
+            holder.binding.tvTitle.text = article.title
+            holder.binding.tvDescription.text = article.description
+            holder.binding.tvPublishedAt.text = article.publishedAt
+            setOnClickListener {
+                onItemClickListener?. let { it(article) }
+            }
+        }
+    }
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
+
+}
