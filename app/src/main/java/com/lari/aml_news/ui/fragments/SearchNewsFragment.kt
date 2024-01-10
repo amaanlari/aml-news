a58fe1a068888c3db8f230db915f3930299b3c47
amaanlari <amaanlari@outlook.comm>
2024-01-10 21:42:57 +0530

    Implement searching news functionality.

--- app/src/main/java/com/lari/aml_news/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/lari/aml_news/ui/fragments/SearchNewsFragment.kt
@@ -1,16 +1,31 @@
 package com.lari.aml_news.ui.fragments
 
-import android.os.Bundle
+ import android.content.Context
+ import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+ import android.view.inputmethod.InputMethodManager
+ import androidx.core.widget.addTextChangedListener
+ import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.lari.aml_news.adapters.NewsAdapter
 import com.lari.aml_news.databinding.FragmentSearchNewsBinding
 import com.lari.aml_news.ui.NewsActivity
 import com.lari.aml_news.ui.NewsViewModel
+import com.lari.aml_news.util.Constants.Companion.SEARCH_NEWS_TIME_DELAY
+import com.lari.aml_news.util.Resource
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SearchNewsFragment : Fragment() {
 
+    val TAG: String = "SearchNewsFragment"
+    lateinit var newsAdapter: NewsAdapter
     private var _binding : FragmentSearchNewsBinding? = null
 
     private val binding get() = _binding!!
@@ -31,6 +46,63 @@ class SearchNewsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         // Instantiate the view model using NewsActivity's view model.
         viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        binding.tilSearch.setEndIconOnClickListener {
+            binding.tietSearch.clearFocus()
+            val inputMethodManager = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(binding.root.windowToken, 0)
+        }
+
+
+        var job: Job? = null
+        binding.tietSearch.addTextChangedListener { editable ->
+            job?.cancel()
+            job = MainScope().launch {
+                delay(SEARCH_NEWS_TIME_DELAY)
+                editable?.let {
+                    if (editable.toString().isNotEmpty()) {
+                        viewModel.searchNews(editable.toString())
+                    }
+                }
+            }
+        }
+
+        viewModel.searchNews.observe(viewLifecycleOwner, Observer {response ->
+            when(response) {
+                is Resource.Success -> {
+                    hideProgessBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgessBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "An error occured : $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgessBar()
+                }
+            }
+        })
+    }
+
+    private fun hideProgessBar() {
+        binding.paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgessBar() {
+        binding.paginationProgressBar.visibility = View.VISIBLE
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        binding.rvSearchNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/lari/aml_news/util/Constants.kt
+++ app/src/main/java/com/lari/aml_news/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "b34cc9cbe5624a83bf37d34dfa062e26"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
